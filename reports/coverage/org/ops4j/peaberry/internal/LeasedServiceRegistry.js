3eac7ac578cdca693ace68fe990a8f2d1b54edb3
Stuart McCulloch <mcculls@gmail.com>
2008-04-08 17:05:58 +0000

    Latest reports

---
+++ reports/coverage/org/ops4j/peaberry/internal/LeasedServiceRegistry.js
@@ -0,0 +1,125 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = { "classes" : [
+    {"id" : 70, "sl" : 31, "el" : 78, "name" : "LeasedServiceRegistry",
+    "methods" : [
+              {"sl" : 40, "el" : 43, "sc" : 3},  {"sl" : 45, "el" : 77, "sc" : 3}  ]}
+    
+ ]
+};
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {
+		"test_14" : {
+					  "name" : "testMultiService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 45 },],
+					  "statements" : [{"sl": 47 },{"sl": 50 },{"sl": 51 },{"sl": 56 },{"sl": 57 },{"sl": 58 },{"sl": 59 },{"sl": 64 },{"sl": 65 },{"sl": 67 },{"sl": 71 },{"sl": 76 },]
+					  },
+		"test_20" : {
+					  "name" : "staticMultiService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 45 },],
+					  "statements" : [{"sl": 47 },{"sl": 50 },{"sl": 51 },{"sl": 56 },{"sl": 57 },{"sl": 58 },{"sl": 59 },{"sl": 64 },{"sl": 65 },{"sl": 67 },{"sl": 71 },{"sl": 76 },]
+					  },
+		"test_17" : {
+					  "name" : "staticUnaryService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 45 },],
+					  "statements" : [{"sl": 47 },{"sl": 50 },{"sl": 51 },{"sl": 56 },{"sl": 57 },{"sl": 58 },{"sl": 59 },{"sl": 64 },{"sl": 65 },{"sl": 67 },{"sl": 71 },{"sl": 76 },]
+					  },
+		"test_16" : {
+					  "name" : "leasedUnaryService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 45 },],
+					  "statements" : [{"sl": 47 },{"sl": 50 },{"sl": 51 },{"sl": 56 },{"sl": 57 },{"sl": 58 },{"sl": 59 },{"sl": 64 },{"sl": 65 },{"sl": 67 },{"sl": 71 },{"sl": 76 },]
+					  },
+		"test_6" : {
+					  "name" : "leasedMultiService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 45 },],
+					  "statements" : [{"sl": 47 },{"sl": 50 },{"sl": 51 },{"sl": 56 },{"sl": 57 },{"sl": 58 },{"sl": 59 },{"sl": 64 },{"sl": 65 },{"sl": 67 },{"sl": 71 },{"sl": 76 },]
+					  }
+ };
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [  [],   [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [  ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [  ] ,
+  [  ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [  ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [ 17 , 6 , 14 , 16 , 20   ] ,
+  [  ] ,
+  [  ] 
+];
---
+++ reports/coverage/org/ops4j/peaberry/internal/ServiceProxyFactory.js
@@ -0,0 +1,151 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = { "classes" : [
+    {"id" : 198, "sl" : 33, "el" : 86, "name" : "ServiceProxyFactory",
+    "methods" : [
+              {"sl" : 36, "el" : 36, "sc" : 3},  {"sl" : 47, "el" : 66, "sc" : 3},  {"sl" : 52, "el" : 57, "sc" : 7},  {"sl" : 77, "el" : 85, "sc" : 3},  {"sl" : 81, "el" : 83, "sc" : 7}  ]}
+    
+ ]
+};
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {
+		"test_15" : {
+					  "name" : "testUnaryService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 52 },],
+					  "statements" : [{"sl": 56 },]
+					  },
+		"test_14" : {
+					  "name" : "testMultiService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 81 },],
+					  "statements" : [{"sl": 82 },]
+					  },
+		"test_12" : {
+					  "name" : "unleasedUnaryService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 52 },],
+					  "statements" : [{"sl": 56 },]
+					  },
+		"test_20" : {
+					  "name" : "staticMultiService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 81 },],
+					  "statements" : [{"sl": 82 },]
+					  },
+		"test_22" : {
+					  "name" : "checkInjection",
+					  "pass" : true ,
+					  "methods" : [{"sl": 52 },{"sl": 81 },],
+					  "statements" : [{"sl": 56 },{"sl": 82 },]
+					  },
+		"test_17" : {
+					  "name" : "staticUnaryService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 52 },],
+					  "statements" : [{"sl": 56 },]
+					  },
+		"test_16" : {
+					  "name" : "leasedUnaryService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 52 },],
+					  "statements" : [{"sl": 56 },]
+					  },
+		"test_6" : {
+					  "name" : "leasedMultiService",
+					  "pass" : true ,
+					  "methods" : [{"sl": 81 },],
+					  "statements" : [{"sl": 82 },]
+					  }
+ };
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [  [],   [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [ 17 , 22 , 12 , 15 , 16   ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [ 17 , 22 , 12 , 15 , 16   ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [ 22 , 6 , 14 , 20   ] ,
+  [ 22 , 6 , 14 , 20   ] ,
+  [  ] ,
+  [  ] ,
+  [  ] ,
+  [  ] 
+];
