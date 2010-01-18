9dce40cac0a10a976331dbc84afc5661254a5143
Todor Boev <rinsvind@gmail.com>
2010-01-18 18:54:03 +0000

    Should have been a tag

--- activation.configadmin-module/main/src/main/java/org/ops4j/peaberry/activation/builders/ConfigurableKeyBuilder.java
+++
@@ -1,7 +0,0 @@
-package org.ops4j.peaberry.activation.builders;
-
-import com.google.inject.Provider;
-
-public interface ConfigurableKeyBuilder<T> { 
-  Provider<T> named(String key);
-}
--- activation.configadmin-module/main/src/main/java/org/ops4j/peaberry/activation/builders/ConfigurablePidBuilder.java
+++
@@ -1,5 +0,0 @@
-package org.ops4j.peaberry.activation.builders;
-
-public interface ConfigurablePidBuilder<T> { 
-  ConfigurableKeyBuilder<T> from(String pid);
-}
